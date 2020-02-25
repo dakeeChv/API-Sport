f41f33071c69bd1282f8c4d2c962d88514bc57a6
dakeeChv <dkartree@gamil.com>
2020-02-25 18:22:17 +0700

    set

--- src/routes/Type.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const { auth } = require('../middleware/isAuth')
-
-const { read } = require('../controller/Type')
-
-router.get('/', auth, read)
-
-module.exports = router
